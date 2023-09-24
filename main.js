173d6600d813257ded2ebde7d56ab76ebe82c297
RenzoSC <76189720+RenzoSC@users.noreply.github.com>
2023-09-23 21:49:03 -0300

    config

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
-
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
 const Client = new Discord.Client(
     {
-        intents:28397
-    }
+        intents:28397,
+    };
 )
 
 Client.on('ready', async( client )=>{
-    console.log("Estoy listo!");
+    console.log('Estoy listo!');
 })
 
-Client.login("MTE1MzE0OTY1Njk5NDQxNDY1NA.GfQv04.Y3OazK9Od-5g03iGj5ZkLg1tcb6tENxAlOrIs0");
+Client.login(apiKey);
