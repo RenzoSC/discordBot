6d792242bfca27f4351a40765dfc2b642aeabcd4
RenzoSC <76189720+RenzoSC@users.noreply.github.com>
2023-11-25 02:18:01 -0300

    added couple command

--- commands/fun/couple.js
+++ commands/fun/couple.js
@@ -3,6 +3,7 @@ const Canvas = require('@napi-rs/canvas');
 
 function gif_to_png(url){
     let newurl = url;
+
     if (newurl.includes('.gif')){
         newurl = url.replace('.gif', '.png');
     }
@@ -12,23 +13,62 @@ function gif_to_png(url){
 module.exports ={
     data : new SlashCommandBuilder()
         .setName('couple')
-        .setDescription('Tag a friend to play rock, paper, scissors!')
+        .setDescription('Tag a friend!')
         .addUserOption((option) => option.setName('friend').setDescription('Tag your friend!').setRequired(true)),
 
     async execute(interaction){
-        const canvas = Canvas.createCanvas(700,300);
+        const canvas = Canvas.createCanvas(700,400);
         const context = canvas.getContext("2d");
         
-        const user_img = gif_to_png(interaction.user.avatarURL({extension: 'png'}));
-        const target_img = gif_to_png(interaction.options.getUser('friend').avatarURL({extension: 'png'}))
+        let user_img = gif_to_png(interaction.user.avatarURL({extension: 'png'}));
+        let target_img = gif_to_png(interaction.options.getUser('friend').avatarURL({extension: 'png'}))
 
         console.log(user_img);
         console.log(target_img);
-
         const background = await Canvas.loadImage('./images/uwu.jpg');
 
+        const heart = await Canvas.loadImage('./images/heart.png');
+
+        user_img = await Canvas.loadImage(user_img);
+        target_img = await Canvas.loadImage(target_img);
+
+        context.imageSmoothingEnabled = true; // Habilitar el antialiasing
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
-        
+
+        // Dibujar el marco circular para la imagen del usuario
+        context.save(); // Guardar el estado actual del contexto
+        context.beginPath();
+        context.arc(125, canvas.height / 2 , 100, 0, Math.PI * 2, true);
+        context.closePath();
+        context.clip(); // Aplicar el recorte al círculo
+
+        // Dibujar la imagen del usuario
+        context.drawImage(user_img, 25, canvas.height / 2 - 100, 200, 200);
+
+        context.restore(); // Restaurar el estado original del contexto
+
+        // Dibujar el marco circular para la imagen del amigo
+        context.save(); // Guardar el estado actual del contexto
+        context.beginPath();
+        context.arc(canvas.width - 125, canvas.height / 2, 100, 0, Math.PI * 2, true);
+        context.closePath();
+        context.clip(); // Aplicar el recorte al círculo
+
+        // Dibujar la imagen del amigo
+        context.drawImage(target_img, canvas.width - 225, canvas.height / 2 - 100, 200, 200);
+
+        context.restore(); // Restaurar el estado original del contexto
+
+        context.save(); // Guardar el estado actual del contexto
+        context.drawImage(heart, canvas.width/2 -55, canvas.height/2 -55, 110,100);
+        context.restore();
+
+        // Agregar texto uwu en el centro del lienzo
+        context.font = '30px Arial';
+        context.fillStyle = '#000000'; // Color del texto
+        context.textAlign = 'center';
+        context.fillText('uwu', canvas.width / 2, canvas.height / 2 + 150);
+
         const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
 
         interaction.reply({files:[attachment]});
